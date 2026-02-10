420bf45f709db4d9b4b9aba98eff776976d29d02
symonmuchemi <simonkenya846@gmail.com>
2026-02-10 08:32:13 +0300

    feat: Update app description for clarity and detail
    Enhanced the app_description in hooks.py to provide a comprehensive overview of the application's functionality, emphasizing its role in automating tax compliance, e-invoicing, and regulatory reporting for Nigerian businesses through integration with Nigeria's Federal Inland Revenue Service (FIRS) via the Digitax Nigeria API.

--- nigeria_compliance_via_digitax/hooks.py
+++ nigeria_compliance_via_digitax/hooks.py
@@ -1,7 +1,7 @@
 app_name = "nigeria_compliance_via_digitax"
 app_title = "Nigeria Compliance Via Digitax"
 app_publisher = "Navari Limited"
-app_description = "Federal Inland Revenue Service (FIRS) integration via Digitax by Navari Ltd for Nigeria."
+app_description = "An ERPNext application for seamless integration with Nigeria's Federal Inland Revenue Service (FIRS) through the Digitax Nigeria API. This app automates tax compliance, e-invoicing, and regulatory reporting for Nigerian businesses."
 app_email = "solutions@navari.co.ke"
 app_license = "mit"
 
