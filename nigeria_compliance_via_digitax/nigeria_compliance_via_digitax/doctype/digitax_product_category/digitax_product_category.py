a6d0fd690727a7f36188a22609d927aa2c1d21c4
symonmuchemi <simonkenya846@gmail.com>
2026-02-16 15:03:12 +0300

    feat: Enhance Digitax HS Code and Product Category functionality
    - Added 'unique' constraint to the 'HS Code' field in the Digitax HS Code doctype to ensure distinct entries.
    - Implemented 'before_save' method in the Digitax Product Category class to set 'is_service' based on the category of the associated HS Code, improving data integrity and functionality.

--- nigeria_compliance_via_digitax/nigeria_compliance_via_digitax/doctype/digitax_product_category/digitax_product_category.py
+++ nigeria_compliance_via_digitax/nigeria_compliance_via_digitax/doctype/digitax_product_category/digitax_product_category.py
@@ -1,9 +1,14 @@
 # Copyright (c) 2026, Navari Limited and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class DigitaxProductCategory(Document):
-	pass
+    def before_save(self):
+        commodity_type = frappe.db.get_value(
+            "Digitax HS Code", self.hs_code, "category"
+        )
+
+        self.is_service = commodity_type == "Service"
