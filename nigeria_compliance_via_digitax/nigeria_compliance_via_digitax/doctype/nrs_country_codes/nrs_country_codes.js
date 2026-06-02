bb236fdcb0e195d5da2f220b80806bc08260dd64
symonmuchemi <simonkenya846@gmail.com>
2026-06-02 10:02:31 +0300

    feat: Introduce NRS Country Codes and remove FIRS Country Codes
    This commit adds a new feature for NRS Country Codes, including the creation of necessary files such as __init__.py, nrs_country_codes.js, nrs_country_codes.json, nrs_country_codes.py, and test_nrs_country_codes.py. The previous FIRS Country Codes files have been removed to streamline the codebase and replace the outdated references with the new NRS implementation.

--- nigeria_compliance_via_digitax/nigeria_compliance_via_digitax/doctype/nrs_country_codes/nrs_country_codes.js
+++ nigeria_compliance_via_digitax/nigeria_compliance_via_digitax/doctype/nrs_country_codes/nrs_country_codes.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2026, Navari Limited and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("FIRS Country Codes", {
+// frappe.ui.form.on("NRS Country Codes", {
 // 	refresh(frm) {
 
 // 	},
